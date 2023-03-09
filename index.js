41f3cd3acee75894bf17dccc90573ff4685b2f51
Naomi Mardon <naomimardon@Naomis-MBP.broadband>
2023-03-09 21:18:23 +0000

    updated index.js with all questions to be prompted in the console

--- index.js
+++ index.js
@@ -13,3 +13,84 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+// questions about a manager
+const managerQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Enter your name:',
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'Enter your employee ID:',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:',
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: 'Enter your office number:',
+    },
+]
+
+// question to ask the user what they want to do after creating manager profile
+const selectNextChoice = [
+    {
+        type: 'list',
+        name: 'choice',
+        message: 'What would you like to do next?',
+        choices: ['Add an engineer', 'Add an intern', 'Finish building the team']
+    },
+]
+
+// questions about an engineer
+const engineerQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Enter your name:',
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'Enter your employee ID:',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your Github username:',
+    },
+]
+
+// questions about an intern
+const internQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Enter your name:',
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'Enter your employee ID:',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:',
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: 'Enter the name of your school:',
+    },
+]
